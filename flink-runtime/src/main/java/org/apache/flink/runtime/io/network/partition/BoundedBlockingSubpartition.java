8e11c44bc14d0bbc0c22a359d3f9d3ff0dd8b862
Anton Kalashnikov <kaa.dev@yandex.ru>
2021-11-08 16:56:41 +0100

    [refactor][runtime] Added postfix `unsafe` for methods ResultSubpartition#getTotalNumberOfBuffers and ResultSubpartition#getTotalNumberOfBytes

--- flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/BoundedBlockingSubpartition.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/BoundedBlockingSubpartition.java
@@ -282,12 +282,12 @@ final class BoundedBlockingSubpartition extends ResultSubpartition {
     }
 
     @Override
-    protected long getTotalNumberOfBuffers() {
+    protected long getTotalNumberOfBuffersUnsafe() {
         return numBuffersAndEventsWritten;
     }
 
     @Override
-    protected long getTotalNumberOfBytes() {
+    protected long getTotalNumberOfBytesUnsafe() {
         return data.getSize();
     }
 
